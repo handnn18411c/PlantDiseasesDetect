f59bda9c279404f1af4393767e288222f3f8e15e
Han_HRAI <handnn@gbee.edu.vn>
2021-07-18 23:35:54 +0700

    ﻿Commit function Camera

--- app/src/main/java/nhathando/com/plantdiseasesdetect/util/Constant.java
+++ app/src/main/java/nhathando/com/plantdiseasesdetect/util/Constant.java
@@ -2,5 +2,9 @@ package nhathando.com.plantdiseasesdetect.util;
 
 public class Constant {
     public static final String KEY_EXTRA = "key_extra";
-    public static final String KEY_IMAGE = "key_image";
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final int PICK_IMAGE = 2 ;
+    public static final String IMAGE_CAPTURE_URI = "image_uri";
+    public static final String KEY_CAMERA_OR_GALLERY = "key_function";
+
 }
