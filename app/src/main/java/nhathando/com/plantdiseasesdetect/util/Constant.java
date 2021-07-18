ccabc31ff6e09a62735c68793830f7e4948dfafc
duy <ASUS@LAPTOP-A3IJCQIJ>
2021-07-18 23:36:45 +0700

    complete choose image and move to ImagePreview Activity

--- app/src/main/java/nhathando/com/plantdiseasesdetect/util/Constant.java
+++ app/src/main/java/nhathando/com/plantdiseasesdetect/util/Constant.java
@@ -2,4 +2,5 @@ package nhathando.com.plantdiseasesdetect.util;
 
 public class Constant {
     public static final String KEY_EXTRA = "key_extra";
+    public static final String KEY_IMAGE = "key_image";
 }
--- app/src/main/java/nhathando/com/plantdiseasesdetect/views/HomeActivity.java
+++ app/src/main/java/nhathando/com/plantdiseasesdetect/views/HomeActivity.java
@@ -2,14 +2,26 @@ package nhathando.com.plantdiseasesdetect.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import nhathando.com.plantdiseasesdetect.R;
+import nhathando.com.plantdiseasesdetect.views.ImagePreview.ImagePreviewActivity;
+
+import static nhathando.com.plantdiseasesdetect.util.Constant.KEY_IMAGE;
+
 
 public class HomeActivity extends BaseActivity {
-
+    @BindView(R.id.btnGallery) Button btnGallery;
+    int PICK_IMAGE = 1 ;
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
@@ -22,10 +34,32 @@ public class HomeActivity extends BaseActivity {
     }
 
     private void addEvents() {
-    
-    }
+        // btnGallery
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, PICK_IMAGE);
 
+            }
+        });
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && data != null) {
+
+            Uri selectedImage = data.getData();
+            Log.d("aaaaaa" , selectedImage.toString());
+            Bundle bundle = new Bundle() ;
+            bundle.putParcelable(KEY_IMAGE , selectedImage);
+            showActivity(ImagePreviewActivity.class ,bundle);
+        }
+    }
     private void addView() {
 
     }
+
 }
