b12e36b2c65e8336827569b582c3ded131e40c8a
duy <ASUS@LAPTOP-A3IJCQIJ>
2021-07-18 23:36:45 +0700

    complete choose image and move to ImagePreview Activity

--- app/src/main/java/nhathando/com/plantdiseasesdetect/views/ImagePreview/ImagePreviewActivity.java
+++ app/src/main/java/nhathando/com/plantdiseasesdetect/views/ImagePreview/ImagePreviewActivity.java
@@ -3,13 +3,27 @@ package nhathando.com.plantdiseasesdetect.views.ImagePreview;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+
+import butterknife.BindView;
 import nhathando.com.plantdiseasesdetect.R;
+import nhathando.com.plantdiseasesdetect.util.Constant;
 import nhathando.com.plantdiseasesdetect.views.BaseActivity;
+
+import static nhathando.com.plantdiseasesdetect.util.Constant.KEY_IMAGE;
 
 public class ImagePreviewActivity extends BaseActivity {
 
+    @BindView(R.id.imgImage)  ImageView imgImage ;
     private ImagePreviewViewModel imagePreviewViewModel;
 
     @Override
@@ -20,6 +34,27 @@ public class ImagePreviewActivity extends BaseActivity {
     @Override
     protected void createView() {
         initView();
+
+        Bundle bundleFromGallery = getIntent().getBundleExtra(Constant.KEY_EXTRA);
+        if(bundleFromGallery != null ) {
+            Uri imagePath = bundleFromGallery.getParcelable(KEY_IMAGE) ;
+            if(imagePath != null ) {
+                Bitmap bitmap= null;
+                try {
+                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imagePath));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                imgImage.setImageBitmap(bitmap);
+                Log.d("aaaaaa" , imagePath.toString());
+            }
+            else Log.d("aaaaaa" , "failedd" ) ;
+
+        }
+
+
+
+
     }
 
     private void initView() {
